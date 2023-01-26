aa5f6607d260e8f662c4a4ee6dc9d5ecbed6e079
O. Riastanjung <oriastan999@gmail.com>
2023-01-26 15:02:33 +0700

    setup sequelize for service-media

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config()
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const mediaRouter = require('./routes/media');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/media', mediaRouter);
+
+module.exports = app;
