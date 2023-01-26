64e11732a6ca298b5d0b1cdd51e964a08e26f422
O. Riastanjung <oriastan999@gmail.com>
2023-01-26 15:33:28 +0700

    api post media img

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const isBase64 = require("is-base64");
+const router = express.Router();
+const base64Img = require("base64-img");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { Media } = require("../models");
+
+router.post("/", (req, res) => {
+  const { image } = req.body;
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: "error", message: "invalid base64" });
+  }
+  base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+
+    // '/public/images/4124124141.png'
+    // const filename = filepath.split('\\').pop().split("/").pop()    // for windows
+    const filename = filepath.split("/").pop(); // for mac
+    const media = await Media.create({
+      image : `image/${filename}`
+    })
+
+    return res.json({
+      status : "success",
+      data : {
+        id : media.id,
+        image: `${req.get('host')}/images/${filename}`
+      }
+    })
+  });
 });
 
 module.exports = router;
