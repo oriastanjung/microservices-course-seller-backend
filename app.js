f9ef281cf244a9d797d98b030532429e33fbdb30
O. Riastanjung <oriastan999@gmail.com>
2023-01-26 14:25:30 +0700

    new setup api-gateway

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const paymentsRouter = require('./routes/payments');
+const ordersRouter = require('./routes/orders');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/media', mediaRouter);
+app.use('/courses', coursesRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
+
+module.exports = app;
