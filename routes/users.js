010b2684a8e615403f560aca7324c2e70411ffc5
O. Riastanjung <oriastan999@gmail.com>
2023-02-03 19:15:40 +0700

    Integrating API USERS Login And Register from service-user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {register, login} = require('./handler/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', register)
+router.post('/login', login)
 
 module.exports = router;
